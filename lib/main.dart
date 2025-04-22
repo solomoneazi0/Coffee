99f197c425f34fb48a3e77b0d2c6ebd7b131b068
solomoneazi0 <noblesaint20@gmail.com>
2025-04-22 21:18:02 +0100

    changed boolean value

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(debugShowCheckedModeBanner: false);
+    return const MaterialApp(debugShowCheckedModeBanner: true);
   }
 }
